19b3b1eb8c85e86bfbb5d08731e5e40640927793
Wondernuttz <borjajf@gmail.com>
2026-02-12 01:26:46 -0500

    Delete OpenCompositeInput directory

--- OpenCompositeInput/src/PCH.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Include CommonLibVR's precompiled header
-#include <SKSE/Impl/PCH.h>
-
-// Bring std::literals into global scope (needed by auto-generated plugin info file)
-using namespace std::literals;
